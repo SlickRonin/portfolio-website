7aa5176f9ebd3925371b308caa2c14abd833c95f
Liam Wilson <liam.a.wilson985@gmail.com>
2025-03-12 16:54:26 -0400

    Better Watch

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,30 @@
-setInterval(setClock,1000)
+setInterval(setClock, 1000);
 
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+const dateElement = document.querySelector('.date'); // Reference to the date element
 
 function setClock() {
-    const currentDate =  new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand,hoursRatio)
-}
+    const currentDate = new Date();
+    
+    // Get the current day (1 to 31)
+    const currentDay = currentDate.getDate();
+    
+    // Update the date element with the current day
+    dateElement.textContent = currentDay;
+    
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
 
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
 
 function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360)
+    element.style.setProperty('--rotation', rotationRatio * 360);
 }
 
-setClock()
+setClock(); // Initialize the clock
